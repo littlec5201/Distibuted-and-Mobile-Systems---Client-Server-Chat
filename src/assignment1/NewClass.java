3599bdaa19c9080ffd0b5b0661be1cdbf7c0c1df
littlec5201 <cjbensemann@gmail.com>
2017-11-13 15:00:50 +1300

    Assignment Submission

---
+++ src/assignment1/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Callum
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
